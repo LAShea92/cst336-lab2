b2baa2b0e50d0e72aafb2a99509500de4719ac88
lshea <lshea@csumb.edu>
2019-07-06 13:56:07 -0400

    This is one in many

--- js/hangman.js
+++ js/hangman.js
@@ -1,20 +1,33 @@
+// VARIABLES
 var selectedWord = "";
 var selectedHint = "";
 var board = [];
 var remainingGuesses = 6;
 var words = ["snake", "monkey", "beetle"];
 
-console.log(words[0]);
+//LISTENERS
+window.onload = startGame();
 
-var randomInt = Math.floor(Math.random() * words.length);
-selectedWord = words[randomInt];
+//FUNCTIONS
+function startGame(){
+    pickWord();
+    initBoard();
+    updateBoard();
+}
 
 function initBoard(){
     for (var letter in selectedWord){
         board.push("_");
     }
 }
-initBoard();
-for(var letter of board){
-    document.getElementById("word").innerHTML += letter + "";
+
+function pickWord(){
+    var randomInt = Math.floor(Math.random() * words.length);
+    selectedWord = words[randomInt];
+}
+
+function updateBoard(){
+    for(var letter of board){
+        document.getElementById("word").innerHTML += letter + " ";
+    }
 }
